ac722162b8a591d7e68d4ae74570223539a371f9
Fengren1998 <rinneeeeeeeeee@gmail.com>
2020-03-02 22:09:59 +0800

    initial commit

---
+++ Components/Map.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using SadConsole;
+using Console = SadConsole.Console;
+using Directions = SadConsole.Directions;
+using SadConsole.Input;
+using Microsoft.Xna.Framework;
+using Colors = Microsoft.Xna.Framework.Color;
+using Keys = Microsoft.Xna.Framework.Input.Keys;
+
+namespace the_invincible_overlord.Components {
+    class Map : ContainerConsole {
+        List<Entity> EntityList;
+        public Console MapConsole;
+        public Cell PlayerGlyph;
+
+        private Point _playerPosition;
+        public Point PlayerPosition {
+            get => _playerPosition;
+            private set {
+                MapConsole.Clear(_playerPosition.X, _playerPosition.Y);
+                _playerPosition = value;
+                PlayerGlyph.CopyAppearanceTo(MapConsole[_playerPosition.X, _playerPosition.Y]);
+            }
+        }
+
+        public Map(int width = 0, int height = 0) {
+            if (width <= 0 && height <= 0) {
+                width = Global.RenderWidth / Global.FontDefault.Size.X;
+                height = Global.RenderHeight / Global.FontDefault.Size.Y;
+            }
+
+            // # Setup the map
+            this.MapConsole = new Console(width, height);
+            this.MapConsole.Parent = this;
+
+            // # Setup the player
+            this.PlayerGlyph = new Cell(Colors.White, Color.Transparent, '@');
+            PlayerPosition = new Point(4, 4);
+            PlayerGlyph.CopyAppearanceTo(MapConsole[PlayerPosition.X, PlayerPosition.Y]);
+        }
+
+        public override bool ProcessKeyboard(Keyboard info) {
+            Point newPlayerPosition = PlayerPosition;
+
+            // # WASD for movement, separate horizontal and vertical to allow diagonal movement
+            if (info.IsKeyPressed(Keys.W)) {
+                newPlayerPosition += Directions.North;
+            } else if (info.IsKeyPressed(Keys.S)) {
+                newPlayerPosition += Directions.South;
+            } else if (info.IsKeyPressed(Keys.A)) {
+                newPlayerPosition += Directions.West;
+            } else if (info.IsKeyPressed(Keys.D)) {
+                newPlayerPosition += Directions.East;
+            }
+
+            // # If values are different, change PlayerPosition's
+            if (newPlayerPosition != PlayerPosition) {
+                PlayerPosition = newPlayerPosition;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using SadConsole;
+using Console = SadConsole.Console;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace the_invincible_overlord {
+    class Program {
+        public const int Width = 80;
+        public const int Height = 50;
+        static Engine MainEngine = new Engine(Width, Height);
+
+        static void Main() {
+            // Setup the engine and create the main window.
+            SadConsole.Game.Create(Width, Height);
+
+            // Hook the start event so we can add consoles to the system.
+            SadConsole.Game.OnInitialize = Init;
+
+            // Start the game.
+            SadConsole.Game.Instance.Run();
+            SadConsole.Game.Instance.Dispose();
+        }
+
+        private static void Init() {
+            MainEngine.Main();
+            SadConsole.Global.CurrentScreen = MainEngine.GetMap(0);
+            Global.CurrentScreen.IsFocused = true;
+        }
+
+        //private static void Init() {
+        //    // Any startup code for your game. We will use an example console for now
+        //    var startingConsole = SadConsole.Global.CurrentScreen;
+        //    startingConsole.FillWithRandomGarbage();
+        //    startingConsole.Fill(new Rectangle(3, 3, 27, 5), null, Color.Black, 0, SpriteEffects.None);
+        //    startingConsole.Print(6, 5, "Hello from SadConsole", ColorAnsi.CyanBright);
+        //}
+    }
+}
